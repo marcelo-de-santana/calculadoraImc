356c7ee76095cc1e4343d94849271611ce8197d2
marcelo-de-santana <marcelondesantana@outlook.com>
2022-08-31 23:06:54 -0300

    Aula 04 parte 2 (concluída)

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    resultImc: {
+    contextImc: {
         flex: 1,
         marginTop: 15,
         paddingTop: 60,
@@ -14,11 +14,27 @@ const styles = StyleSheet.create({
         color: "#ff0043",
         fontWeight: "bold",
     },
-    information: {
+    titleResultImc: {
         fontSize: 18,
         color: "#ff0043",
         fontWeight: "bold",
     },
+    boxShareButtom: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    shared: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    sharedText: {
+        color: "ffffff",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
+    }
 });
 
 export default styles;
